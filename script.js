892482f72545a9014ff45419a38dbe36c345369d
spcameron <cameron.seanpatrick@gmail.com>
2025-05-20 21:00:49 -0400

    Implement getComputerChoice() function
    "Step 2" of the project outline.
    
    Creates a function with randomly returns either "rock", "paper", or
    "scissors".

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+function getComputerChoice() {
+  let computerChoice;
+  const randomValue = Math.floor(Math.random() * 3) + 1
+  if (randomValue === 1) {
+    computerChoice = "rock";
+  } else if (randomValue === 2) {
+    computerChoice = "paper";
+  } else if (randomValue === 3) {
+    computerChoice = "scissors";
+  }
+  return computerChoice;
+}
+
 const log = console.log;
-log("Hello World");
+// log("Hello World");
+// log(getComputerChoice());
