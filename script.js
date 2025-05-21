dd509aab705073b3ce0f2e55732800e9c41c4633
spcameron <cameron.seanpatrick@gmail.com>
2025-05-21 11:10:43 -0400

    Implement playRound() function
    This is "Step 5" of the project outline.
    
    playRound() takes strings humanChoice and computerChoice and determines
    a winning player. It handles the case of ties as well. In all three cases,
    it prints a message to console with the outcome of the round, and also
    increments the score of the winning player.

--- script.js
+++ script.js
@@ -16,11 +16,46 @@ function getHumanChoice() {
   return humanChoice.toLowerCase();
 }
 
+function playRound(humanChoice, computerChoice) {
+  let humanWins = true;
+
+  if (humanChoice === computerChoice) {
+    return `It's a tie! You both played ${humanChoice}.`;
+  }
+
+  if (
+    (humanChoice === "rock" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "rock")
+  ) {
+    humanWins = false;
+  }
+
+  if (humanWins) {
+    humanScore += 1;
+    return `You win! ${capitalizeFirstLetter(
+      humanChoice
+    )} beats ${computerChoice}.`;
+  } else {
+    computerScore += 1;
+    return `You lose! ${capitalizeFirstLetter(
+      computerChoice
+    )} beats ${humanChoice}.`;
+  }
+}
+
+function capitalizeFirstLetter(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
 let humanScore = 0;
 let computerScore = 0;
-
 
 const log = console.log;
 // log("Hello World");
 // log(getComputerChoice());
 // log(getHumanChoice());
+
+// const humanChoice = getHumanChoice();
+// const computerChoice = getComputerChoice();
+// log(playRound(humanChoice, computerChoice));
