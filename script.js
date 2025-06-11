f08822b21d9847d0365e9d56c884298f704e4e84
spcameron <cameron.seanpatrick@gmail.com>
2025-06-11 14:33:18 -0400

    Refactor js script for UI interaction
    This commit represents major changes to script.js in order to allow for
    the game to be played using the user interface. Rather than initiating a
    new game on page load and using inputs and console to advance the game,
    this script responds entirely to user clicks on the three buttons -
    rock, paper, or scissors. With each click, the player choice is recorded
    and passed to a playRound() function to determine a winner (or tie).
    Each round played updates the global variables of humanScore,
    computerScore, and roundWinner.
    
    After each round, the UI is updated to reflect the player and computer
    choices from the last round, the round result, and the round report
    (e.g. "Rock beats scissors"). Additionally, the scoreboard is updated to
    show the current scores. Finally, each click checks for whether the game
    is over (i.e. either player currently has 5 points). If the game is
    over, an endgame modal is displayed showing either "You won!" or "You
    lost!" and a button to start a new game. By clicking on the button, the
    page is essentially reset to its starting state without needing to
    refresh. Clicking outside of the modal hides it, but selecting one of
    the RPS buttons without starting a new game opens the modal once again.

--- script.js
+++ script.js
@@ -1,89 +1,172 @@
 // GAME
 
+let humanScore = 0;
+let computerScore = 0;
+let roundWinner = "";
+
 function getComputerChoice() {
-  let computerChoice;
   const randomValue = Math.floor(Math.random() * 3) + 1;
-  if (randomValue === 1) {
-    computerChoice = "rock";
-  } else if (randomValue === 2) {
-    computerChoice = "paper";
-  } else if (randomValue === 3) {
-    computerChoice = "scissors";
+  switch (randomValue) {
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    case 3:
+      return "scissors";
   }
-  return computerChoice;
 }
 
-function getHumanChoice() {
-  let humanChoice = prompt("Make your choice: rock, paper, scissors");
-  return humanChoice.toLowerCase();
-}
-
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  let humanChoice;
-  let computerChoice;
-
-  function playRound(humanChoice, computerChoice, roundNumber) {
-    let humanWins = true;
-    let message;
-
-    if (humanChoice === computerChoice) {
-      message = `Round ${roundNumber} is a tie! You both played ${humanChoice}.`;
-      console.log(message);
-      return;
-    }
-
-    if (
-      (humanChoice === "rock" && computerChoice === "paper") ||
-      (humanChoice === "paper" && computerChoice === "scissors") ||
-      (humanChoice === "scissors" && computerChoice === "rock")
-    ) {
-      humanWins = false;
-    }
-
-    if (humanWins) {
-      humanScore += 1;
-      message = `You win round ${roundNumber}! ${capitalizeFirstLetter(
-        humanChoice
-      )} beats ${computerChoice}.`;
-    } else {
-      computerScore += 1;
-      message = `You lose round ${roundNumber}! ${capitalizeFirstLetter(
-        computerChoice
-      )} beats ${humanChoice}.`;
-    }
-
-    console.log(message);
-  }
-
-  for (let i = 0; i < 5; i++) {
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice, i + 1);
-  }
-
-  reportWinner(humanScore, computerScore);
-}
-
-function reportWinner(humanScore, computerScore) {
-  let message = `With a score of ${humanScore} to ${computerScore}, `;
-  if (humanScore > computerScore) {
-    message += `you win!`;
-  } else if (computerScore > humanScore) {
-    message += `you lose!`;
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    roundWinner = "tie";
+  } else if (
+    (humanChoice === "rock" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "rock")
+  ) {
+    computerScore++;
+    roundWinner = "computer";
   } else {
-    message += `it's a tie!`;
+    humanScore++;
+    roundWinner = "human";
   }
-  console.log(message);
+}
+
+function isGameOver() {
+  return humanScore === 5 || computerScore === 5;
+}
+
+// UI
+
+const buttons = document.querySelectorAll(".rps-button");
+const roundHeader = document.querySelector("#roundHeader");
+const roundSubheader = document.querySelector("#roundSubheader");
+const humanSign = document.querySelector("#humanSign");
+const humanScoreboard = document.querySelector("#humanScore");
+const computerSign = document.querySelector("#cpuSign");
+const computerScoreboard = document.querySelector("#cpuScore");
+const endgameModal = document.querySelector("#endgameModal");
+const endgameMessage = document.querySelector("#endgameMessage");
+const newGameButton = document.querySelector("#newGameButton");
+const overlay = document.querySelector(".overlay");
+
+const rockEmoji = "✊";
+const paperEmoji = "✋";
+const scissorsEmoji = "✌";
+
+newGameButton.addEventListener("click", newGame);
+overlay.addEventListener("click", closeModal);
+for (const button of buttons) {
+  button.addEventListener("click", (e) => handleClick(e.target.id));
+}
+
+function handleClick(playerChoice) {
+  if (isGameOver()) {
+    reportWinner();
+    openModal();
+    return;
+  }
+
+  const computerChoice = getComputerChoice();
+  playRound(playerChoice, computerChoice);
+  updateChoices(playerChoice, computerChoice);
+  updateScoreboard(playerChoice, computerChoice);
+
+  if (isGameOver()) {
+    reportWinner();
+    openModal();
+  }
+}
+
+function updateChoices(playerChoice, computerChoice) {
+  switch (playerChoice) {
+    case "rock":
+      humanSign.textContent = rockEmoji;
+      break;
+    case "paper":
+      humanSign.textContent = paperEmoji;
+      break;
+    case "scissors":
+      humanSign.textContent = scissorsEmoji;
+      break;
+  }
+
+  switch (computerChoice) {
+    case "rock":
+      computerSign.textContent = rockEmoji;
+      break;
+    case "paper":
+      computerSign.textContent = paperEmoji;
+      break;
+    case "scissors":
+      computerSign.textContent = scissorsEmoji;
+      break;
+  }
+}
+
+function updateScoreboard(playerChoice, computerChoice) {
+  let header = "";
+  let subheader = "";
+
+  switch (roundWinner) {
+    case "tie":
+      header = "It's a tie!";
+      subheader = `${capitalizeFirstLetter(
+        playerChoice
+      )} ties with ${computerChoice}`;
+      break;
+    case "human":
+      header = "You win this round!";
+      subheader = `${capitalizeFirstLetter(
+        playerChoice
+      )} beats ${computerChoice}`;
+      break;
+    case "computer":
+      header = "You lose this round!";
+      subheader = `${capitalizeFirstLetter(
+        playerChoice
+      )} loses to ${computerChoice}`;
+      break;
+  }
+
+  roundHeader.textContent = header;
+  roundSubheader.textContent = subheader;
+  humanScoreboard.textContent = `PLAYER: ${humanScore}`;
+  computerScoreboard.textContent = `COMPUTER: ${computerScore}`;
+}
+
+function reportWinner() {
+  let message = "";
+  if (humanScore > computerScore) {
+    message = "You won!";
+  } else {
+    message = "You lost!";
+  }
+  endgameMessage.textContent = message;
+}
+
+function openModal() {
+  endgameModal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal() {
+  endgameModal.classList.remove("active");
+  overlay.classList.remove("active");
 }
 
 function capitalizeFirstLetter(word) {
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
-playGame();
-
-// UI
-
+function newGame() {
+  humanScore = 0;
+  computerScore = 0;
+  roundHeader.textContent = "Click below to play!";
+  roundSubheader.textContent = "First to five points wins";
+  humanScoreboard.textContent = `PLAYER: ${humanScore}`;
+  computerScoreboard.textContent = `COMPUTER: ${computerScore}`;
+  humanSign.textContent = "?";
+  computerSign.textContent = "?";
+  closeModal();
+}
